58c0c836a1dceda6706340b9a01c4f24067b1fc7
Jobzzz <jobi.j@fayausa.com>
2023-12-01 13:13:04 +0530

    feat(age-calculator): add code for age calculator;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'age-cal';
+
+  public date = new Date()
+  public intYears: any  = '--'
+  public intMonths: any = '--'
+  public intDays: any = '--'
+  profileForm: FormGroup;
+  isValidDate:boolean = true;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(){
+    this.initForm();
+  }
+
+  initForm(){
+    this.profileForm = this.formBuilder.group({
+      day: ['',[ Validators.required, Validators.min(1), Validators.max(31)]],
+      month: ['',[ Validators.required, Validators.min(1), Validators.max(12) ]],
+      year: ['',[ Validators.required, Validators.min(1),Validators.max(this.date.getFullYear())]],
+    });
+  }
+
+  getErrors(profileFormName:string, validatorName:string) {
+    return this.profileForm.controls[profileFormName].errors?.[validatorName]  
+  }
+
+  calculateUserAge():void {
+    this.intYears = this.date.getFullYear() -  this.profileForm.value.year
+    this.intMonths = this.date.getMonth() + 1 - this.profileForm.value.month
+    this.intDays = Math.abs(this.date.getDate() - this.profileForm.value.day)
+    console.log('YMD',this.date.getDate() , this.profileForm.value.day );
+  }
+
+  isDateValid(){
+    const day = this.profileForm.get('day');
+    const month = this.profileForm.get('month')
+    return (
+      day && month && month.value === 2 && day.value > 28 ||
+      day && month && month.value === 4 && day.value > 30 ||
+      day && month && month.value === 6 && day.value > 30 ||
+      day && month && month.value === 9 && day.value > 30 ||
+      day && month && month.value === 11 && day.value > 30 
+  
+      ? this.isValidDate = false  : this.isValidDate = true
+    )
+  }
+
+  onSubmit():any {
+    console.log(this.profileForm.valid)
+    if (this.profileForm.invalid) {
+      this.profileForm.markAsDirty()
+      return 
+    }
+    this.profileForm.markAsPristine()
+    this.calculateUserAge()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+    // MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
